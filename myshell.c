458c00d04ab866642d07f9df674ffe98ef51457d
Cory <cjl9703@yahoo.com>
2019-02-27 22:15:44 -0800

    Basic I/O with tokenizer

--- myshell.c
+++ myshell.c
@@ -1,7 +1,7 @@
 /****************************************************************
- * Name        :                                                *
+ * Name        :  Cory Lewis                                    *
  * Class       :  CSC 415                                       *
- * Date        :                                                *
+ * Date        :  2/27/19                                       *
  * Description :  Writting a simple bash shell program          *
  *                that will execute simple commands. The main   *
  *                goal of the assignment is working with        *
@@ -16,17 +16,40 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-
 /* CANNOT BE CHANGED */
 #define BUFFERSIZE 256
 /* --------------------*/
 #define PROMPT "myShell >> "
 #define PROMPTSIZE sizeof(PROMPT)
 
+
+/*
+	Citing Sources:
+	//Used this for learning strtok -> https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
+	
+*/
+
 int 
 main(int* argc, char** argv)
 {
 
+   char inputString[BUFFERSIZE];
+   const char delimiter[4] = " ";
+   char *token;
+
+   /* Prompt, scan until line is read */
+   printf(PROMPT);
+   scanf("%[^\n]", inputString);
+
+   /* first token pointer */
+   token = strtok(inputString, delimiter);
+   
+   /* loop through tokens */
+   while( token != 0 ) {
+      printf( " %s\n", token);
+    
+      token = strtok(0, delimiter);
+   }
     
 return 0;
 }
